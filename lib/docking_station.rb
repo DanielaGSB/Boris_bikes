03305d20a8964b20f5ce25c36a19cd760be831f8
Daniela <daniela.galeote@gmail.com>
2015-11-25 18:16:55 +0000

    Up to ch12

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,13 +1,16 @@
 require_relative 'bike'
 
 class DockingStation
+
   attr_reader :bike
+
   def release_bike
-    bike=Bike.new
-    raise "no bikes" if @bike == nil
+    fail 'No bikes available' unless @bike
+    @bike
   end
 
   def dock(bike)
+    fail 'Docking station full' if @bike
     @bike = bike
   end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,23 +1,33 @@
 require 'docking_station'
 describe DockingStation do
-  it { is_expected.to respond_to :release_bike }
   it { is_expected.to respond_to(:dock).with(1).argument }
   it { is_expected.to respond_to :bike}
-  it { is_expected.to raise_error :release_bike}
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
-  end
+
   it 'returns docked bikes' do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.bike).to eq bike
   end
-describe '#release_bike' do
-  it 'releases a bike' do
-  bike= Bike.new
-  subject.dock(bike)
-  expect(subject.release_bike).to eq bike
-end
-end
+
+  describe '#dock' do
+    it 'raises error when docking station is full' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect {subject.dock(bike)}.to raise_error
+    end
+  end
+
+  describe '#release_bike' do
+    it { is_expected.to respond_to :release_bike }
+
+    it 'raises error when no bikes available' do
+      expect {subject.release_bike}.to raise_error
+    end
+
+    it 'releases working bikes' do
+      bike = Bike.new
+      expect(bike).to be_working
+    end
+  end
+
 end
